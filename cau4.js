62d63baafc6a768c690cb055bc2d793d9df000b4
Hung <lebaohung.1996@gmail.com>
2020-03-17 16:19:30 +0700

    nap bai

--- cau4.js
+++ cau4.js
@@ -1,12 +1,12 @@
-function Circle(xPos, yPos, radius ) {
+function Circle(xPos, yPos, radius, color ) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.radius = radius;
-    this.color = "#000000";
+    this.color = color;
 }
 
-Circle.prototype.render = function() {
-    let ctx = document.getElementById("myCanvas").getContext("2d");
+Circle.prototype.render = function(canvasid) {
+    let ctx = document.getElementById(canvasid).getContext("2d");
     ctx.beginPath();
     ctx.arc(this.xPos, this.yPos, this.radius, 0,2*Math.PI);
     ctx.fillStyle = this.color;
@@ -14,5 +14,5 @@ Circle.prototype.render = function() {
 }
 
 
-let myCircle = new Circle(10, 10, 100);
-myCircle.render();
+let myCircle = new Circle(10, 10, 100, "#000000");
+myCircle.render("myCanvas");
